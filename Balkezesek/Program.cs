cee51685931dff58ab509da4704d46a095c3e74e
Doby <d.szabi144@gmail.com>
2021-04-14 18:38:09 +0200

    3.feladat

--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -10,6 +10,7 @@ namespace Balkezesek
     class Program
     {
         static List<Jatekos> jatekosLista = new List<Jatekos>();
+        static List<string> Specjatekosok = new List<string>();
         static void Main(string[] args)
         {
             string[] sor = File.ReadAllLines("balkezesek.csv", Encoding.Default);
@@ -19,8 +20,39 @@ namespace Balkezesek
                 jatekosLista.Add(new Jatekos(sor[i]));
             }
             OsszesVersenyzo();
+            ElsoVersenyzok("1980");
+            BekertSpecJatekos();
 
             Console.ReadLine();
+        }
+
+        private static void BekertSpecJatekos()
+        {
+            Console.Write("Kérek egy játékos nevet: ");
+            string bekertJatekos = Console.ReadLine();
+            double magassag;
+
+            foreach (var jatekos in jatekosLista)
+            {
+                if (Specjatekosok.Contains(bekertJatekos)&& jatekos.nev.Contains(bekertJatekos))
+                {
+                    magassag=jatekos.magassag*2.56;
+                    Console.WriteLine($"A bekert játékos {bekertJatekos} magassága {jatekos.magassag*2.56:N1}");
+                }
+            }
+        }
+
+        private static void ElsoVersenyzok(string adottEv)
+        {
+            Console.WriteLine("Az adott első évükben pályára lépett jatékosok: ");
+            foreach (var jatekos in jatekosLista)
+            {
+                if (jatekos.elso.Contains(adottEv))
+                {
+                    Console.WriteLine($"\t{jatekos.nev}");
+                    Specjatekosok.Add(jatekos.nev);
+                }
+            }
         }
 
         private static void OsszesVersenyzo()

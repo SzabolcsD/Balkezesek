ebb48db4d99058f31fc619b120c6be14cbb1e0be
Doby <d.szabi144@gmail.com>
2021-04-14 18:13:24 +0200

    elsoFeladat

---
+++ Balkezesek/Jatekos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Jatekos
+    {
+        public string nev { get; set; }
+        public string elso { get; set; }
+        public string utolso { get; set; }
+        public int suly { get; set; }
+        public int magassag { get; set; }
+        public Jatekos(string adatsor)
+        {
+            string[] reszek = adatsor.Split(';');
+          
+            nev = reszek[0];
+            elso = reszek[1];
+            utolso = reszek[2];
+            suly = int.Parse(reszek[3]);
+            magassag = int.Parse(reszek[4]);
+        }
+    }
+}
---
+++ Balkezesek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static List<Jatekos> jatekosLista = new List<Jatekos>();
+        static void Main(string[] args)
+        {
+            string[] sor = File.ReadAllLines("balkezesek.csv", Encoding.Default);
+            int sorhossz = sor.Length;
+            for (int i = 1; i < sorhossz; i++)
+            {
+                jatekosLista.Add(new Jatekos(sor[i]));
+            }
+            OsszesVersenyzo();
+
+            Console.ReadLine();
+        }
+
+        private static void OsszesVersenyzo()
+        {
+            int db = jatekosLista.Count;
+            Console.WriteLine($"1. feladat: Összesen {db} versenyző van.");
+        }
+    }
+}
